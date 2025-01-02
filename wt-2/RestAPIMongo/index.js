7711d0068f58cd888c3c10587f3647060dc5b5a0
harmik09 <24010101680@darshan.ac.in>
2025-01-02 09:50:47 +0530

    Add files via upload

---
+++ wt-2/RestAPIMongo/index.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Student = require('./model/Student');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const atlasUrl = "mongodb+srv://student:student123@student.2n4nv.mongodb.net/?retryWrites=true&w=majority&appName=student/Student";
+
+mongoose.connect(atlasUrl).then(() => {
+    console.log("connected to DB Server");
+
+    const app = express();
+
+    app.use(bodyParser.json);
+
+
+    // getAll
+    app.get("/student", async (req, res) => {
+        const data = await Student.find();
+        res.send(data);
+    });
+
+    // getByID
+    app.get("./student/:id", async (req, res) => {
+        const data = await Student.findOne({ _id: req.params.id });
+        res.send(data);
+    });
+
+    // delete
+    app.delete("/student/:id", async (req, res) => {
+        const data = await Student.deleteOne({ _id: req.params.id });
+    })
+
+    // insert(create)
+    app.post("/student", async (req, res) => {
+        const obj = new Student({
+            StudentName: req.body.StudentName,
+            StudentMobile: req.body.StudentMobile,
+            StudentEmail: req.body.StudentEmail
+        });
+
+        const data = await obj.save();
+        res.send(data);
+    });
+
+    // update 
+    app.patch("/student/:id", async(req, res) => {
+        let stu=await Student.findOne({ _id: req.params.id });
+        stu.StudentName= req.body.StudentName;
+        stu.StudentMobile= req.body.StudentMobile;
+        stu.StudentEmail= req.body.StudentEmail;
+    
+        const datad=await stu.save();
+        res.send(data);
+    });
+
+    app.listen(3000, () => {
+        console.log("Server start @ 3000");
+    })
+});
